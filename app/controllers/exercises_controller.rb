44ecba72162f57b01cc460c262417c59cc180938
dibdas <dibyandu08das@gmail.com>
2021-09-01 19:56:49 +0530

    modified exercise controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,30 @@
 class ExercisesController < ApplicationController
+    before_action :authenticate_user, only: %i[show]
+    def index
+        @exercises = Exercise.all
+      end
+    
+      def show
+        @exercise = Exercises.find(params[:id])
+      end
+    
+      def new
+        @article = Exercise.new
+      end
+    
+      def create
+        @article = Exercise.new(exercise_params)
+    
+        if @article.save
+          redirect_to @article
+        else
+          render :new
+        end
+      end
+    
+      private
+        def article_params
+          params.require(:erxercise).permit(:name, :unit)
+        end
+    end
 end
