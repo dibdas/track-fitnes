be0c1b6ee494e5dd292af00c7974700822c63bd2
dibdas <dibyandu08das@gmail.com>
2021-09-27 20:48:21 +0530

    added igration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_163709) do
+ActiveRecord::Schema.define(version: 2021_09_24_225722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2021_09_06_163709) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "exercise_id", null: false
+    t.index ["exercise_id"], name: "index_measurements_on_exercise_id"
     t.index ["user_id"], name: "index_measurements_on_user_id"
   end
 
@@ -37,5 +39,6 @@ ActiveRecord::Schema.define(version: 2021_09_06_163709) do
     t.string "email"
   end
 
+  add_foreign_key "measurements", "exercises"
   add_foreign_key "measurements", "users"
 end
