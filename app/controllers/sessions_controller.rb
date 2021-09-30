9b5435e8ab91479bd712da2a558ea3cfdd81e5bc
dibdas <dibyandu08das@gmail.com>
2021-09-30 19:39:01 +0530

    modified

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,12 @@
 # rubocop : disable Lint/Syntax
-​class SessionsController < ApplicationController
+class SessionsController < ApplicationController
   skip_before_action :authenticate_request
   prepend SimpleCommand
   def new
     render layout: 'slate'
-  end
-  ​
+  end​
   def create
     @user = User.find_by(email: params[:email])
-    ​
     if @user.authenticate(params: [:password])
       token = JsonWebToken.encode(user_id: user.id)
       render json: @user.as_json, status: created, token: token
@@ -16,7 +14,6 @@
       head(:unauthorized)
     end
   end
-  ​
   def destroy
     sign_out_user
     redirect_to root_path
