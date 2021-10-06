05bd156e0e05ca7f76133f79ba97122fd03db684
dibdas <dibyandu08das@gmail.com>
2021-10-06 18:55:14 +0530

    modified
    removed commented code

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 Rails.application.routes.draw do
  
   post '/login', to: 'authentication#authenticate'
-  # post '/create_user', to: 'authentication#authenticate'
   post '/signup',action: :create, controller: 'authentication'
   # resource :authentication
   resources :exercises
