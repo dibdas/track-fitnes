f1c97a5686e84f6302b769c9c3a40b47a58dfd65
dibdas <dibyandu08das@gmail.com>
2021-10-06 18:56:00 +0530

    removed the commented code

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
  
   post '/login', to: 'authentication#authenticate'
   post '/signup',action: :create, controller: 'authentication'
-  # resource :authentication
   resources :exercises
   resources :measurements
   
