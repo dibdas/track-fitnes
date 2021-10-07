4cc5d8796efb03b6ee8aa73738b0bdd0e19540b4
dibdas <dibyandu08das@gmail.com>
2021-10-07 12:54:08 +0530

    renamed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 
 Rails.application.routes.draw do
  
-  post '/login', to: 'authentication#authenticate'
-  post '/signup',action: :create, controller: 'authentication'
+  
+  post '/authenticate', to: 'authentication#authenticate'
+  post '/authentication', action: :create, controller: 'authentication'
   resources :exercises
   resources :measurements
   
