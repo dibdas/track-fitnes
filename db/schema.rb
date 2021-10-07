e98c0cef6ad73c6f82a7b535b34c6e27a9eb0a1d
dibdas <dibyandu08das@gmail.com>
2021-10-07 18:13:38 +0530

    added uniqueness to database level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_225722) do
+ActiveRecord::Schema.define(version: 2021_10_07_124110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2021_09_24_225722) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "measurements", "exercises"
