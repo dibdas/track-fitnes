caa6b88162023190ef91a8d66d7f495ddaa57e31
dibdas <dibyandu08das@gmail.com>
2021-10-07 18:41:31 +0530

    removed unweanted lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,2 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, only: %i[show]
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
-  end
 end
